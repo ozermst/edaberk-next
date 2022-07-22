2de9624d46ce60dcf24f46dbb1556f47dea3157b
Mustafa Ozer <mustafa.ozer@cento.com.tr>
2022-07-22 19:33:25 +0300

    production allowed hosts

--- edaberk/settings/production.py
+++ edaberk/settings/production.py
@@ -2,6 +2,9 @@ from .base import *
 
 DEBUG = False
 
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["188.166.160.83"]
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
